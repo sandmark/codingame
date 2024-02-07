4f380bad74f00c5729b6ce9580b6a44c92d70454
sandmark <sandmark.m@gmail.com>
2024-02-08 01:30:45 +0900

    Solve Power of Thor - Episode 1

--- test/src/codeingame/core_test.clj
+++ test/src/codeingame/core_test.clj
@@ -12,3 +12,14 @@
   (testing "Returns index of max number"
     (is (match? 4
                 (sut/max-index [0 1 2 3 10 5 6 7])))))
+
+(deftest power-of-thor-ep1-test
+  (testing "Read input"
+    (is (match? [[1 2] [3 4]]
+                (with-in-str "1 2 3 4"
+                  (sut/thor-get-input)))))
+  (testing "Compare position"
+    (is (match? [-1 1]
+                (sut/compare-pos [0 2] [1 1])))
+    (is (match? "NW"
+                (sut/thor-direction [1 1] [0 0])))))
