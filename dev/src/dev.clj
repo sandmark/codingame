bb6e452de3832ed264a457aa55ac3eb131c13e30
sandmark <sandmark.m@gmail.com>
2024-02-18 11:18:16 +0900

    Add(dev): require clojure.repl.deps

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -2,6 +2,7 @@
   (:require
    [clojure.java.io :as io]
    [clojure.repl :refer :all]
+   [clojure.repl.deps :refer :all]
    [clojure.tools.namespace.repl :refer [refresh]]))
 
 (clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src" "test/src")
